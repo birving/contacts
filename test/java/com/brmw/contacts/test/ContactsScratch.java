c1257a6a42b57092904ee955993d2ac7ade4119d
Bruce Irving <bi@brmw.com>
2009-04-26 01:55:54 +0000

    Adding test package

---
+++ test/java/com/brmw/contacts/test/ContactsScratch.java
@@ -0,0 +1,13 @@
+package com.brmw.contacts.test;
+// import com.mysql.jdbc.Driver;
+
+/**
+ * Copyright 2009, All rights reserved.
+ * @author Bruce Irving
+ * 
+ * This 
+ */
+public class ContactsScratch {
+    
+    
+}
---
+++ test/java/com/brmw/contacts/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes exclusively for testing purposes.
+ * <p>
+ * Copyright &copy; 2009 Bruce E. Irving. All rights reserved.
+ */
+package com.brmw.contacts.test;
+
