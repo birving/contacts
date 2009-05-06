2f80f5213b8681f1b095018f07e1668919409a01
Bruce Irving <bi@brmw.com>
2009-05-06 19:40:50 +0000

    Added package info.

---
+++ src/java/com/brmw/contacts/hibernate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes associated with Hibernate ORM package.
+ * <p>
+ * Copyright &copy; 2009 Bruce E. Irving. All rights reserved.
+ */
+package com.brmw.contacts.hibernate;
+
--- test/java/com/brmw/contacts/test/ContactsScratch.java
+++ test/java/com/brmw/contacts/test/ContactsScratch.java
@@ -3,10 +3,9 @@ package com.brmw.contacts.test;
 /**
  * @author Bruce Irving
  * 
- * <p/>
- * This is a a scratch class. Probably will be deleted at some point...  
+ *         <p/>
+ *         This is a a scratch class. Probably will be deleted at some point...
  */
 public class ContactsScratch {
-    
-    
+
 }
