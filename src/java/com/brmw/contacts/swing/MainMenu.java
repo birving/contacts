9cccbdeca370809e1d2bc98e6a50862b4ae0cf01
Bruce Irving <bi@brmw.com>
2009-05-17 03:24:15 +0000

    Refactored menu logic for readability.

--- src/java/com/brmw/contacts/swing/MainMenu.java
+++ src/java/com/brmw/contacts/swing/MainMenu.java
@@ -11,31 +11,37 @@ import javax.swing.JMenuItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.brmw.contacts.model.impl.MediaMaintAction;
-import com.brmw.contacts.presenter.MediaMaintPresenter;
-
 public class MainMenu {
     private static final Logger logger = LoggerFactory.getLogger(MainMenu.class);
     private JMenuBar menuBar;
 
     public MainMenu() {
         menuBar = new JMenuBar();
-        initialize();
+        crateMainMenu();
     }
 
-    private void initialize() {
-        /*
-         * Main application menu
-         */
+    public JMenuBar getMenuBar() {
+        return menuBar;
+    }
+
+    /**
+     * Create Main application menu
+     */
+    private void crateMainMenu() {
         JMenu contactsMenu = menuBar.add(new JMenu("Contacts"));
         contactsMenu.setMnemonic(KeyEvent.VK_C);
+        createContactsMenu(contactsMenu);
 
         JMenu toolsMenu = menuBar.add(new JMenu("Tools"));
         toolsMenu.setMnemonic(KeyEvent.VK_T);
+        createToolsMenu(toolsMenu);
 
         JMenu helpMenu = menuBar.add(new JMenu("Help"));
         helpMenu.setMnemonic(KeyEvent.VK_H);
+        createHelpMenu(helpMenu);
+    }
 
+    private void createContactsMenu(JMenu contactsMenu) {
         /*
          * Contacts menu
          */
@@ -56,28 +62,28 @@ public class MainMenu {
                 System.exit(0);
             }
         });
+    }
 
-        /*
-         * Tools menu
-         */
+    /**
+     * Create Tools menu
+     */
+    private void createToolsMenu(JMenu toolsMenu) {
         JMenuItem defineMediaMenuItem = toolsMenu.add(new JMenuItem("Define media"));
         defineMediaMenuItem.setMnemonic(KeyEvent.VK_M);
 //        new MediaMaintPresenter(defineMediaMenuItem, new MediaMaintAction());
         
         JMenuItem defineAssociationsMenuItem = toolsMenu.add(new JMenuItem("Define associations"));
         defineAssociationsMenuItem.setMnemonic(KeyEvent.VK_A);
+    }
 
-        /*
-         * Help menu
-         */
+    /**
+     * Create Help menu
+     */
+    private void createHelpMenu(JMenu helpMenu) {
         JMenuItem helpContentsMenuItem = helpMenu.add(new JMenuItem("Help contents"));
         helpContentsMenuItem.setMnemonic(KeyEvent.VK_H);
 
         JMenuItem aboutMenuItem = helpMenu.add(new JMenuItem("About Contacts application"));
         aboutMenuItem.setMnemonic(KeyEvent.VK_A);
-    }
-
-    public JMenuBar getMenuBar() {
-        return menuBar;
     }
 }
