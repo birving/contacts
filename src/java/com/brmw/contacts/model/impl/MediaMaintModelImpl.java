03dce963013be3bf903936227184df250b8e2694
Bruce Irving <bi@brmw.com>
2009-05-20 04:41:35 +0000

    Completed first story.

--- src/java/com/brmw/contacts/model/impl/MediaMaintModelImpl.java
+++ src/java/com/brmw/contacts/model/impl/MediaMaintModelImpl.java
@@ -1,6 +1,5 @@
 package com.brmw.contacts.model.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.hibernate.Session;
@@ -17,25 +16,8 @@ public class MediaMaintModelImpl implements MediaMaintModel {
         Session session = HibernateFactory.openSession();
         
         @SuppressWarnings("unchecked")
-        Collection<Medium> tableData = session.createQuery("select from Medium").list();
+        Collection<Medium> tableData = session.createQuery("from Medium").list();
         
-//        // Add some test data ...
-//        Collection<Medium> tableData = new ArrayList<Medium>();
-//        Medium medium1 = new Medium();
-//        medium1.setName("Primary email");
-//        medium1.setType("email");
-//        tableData.add(medium1);
-//
-//        Medium medium2 = new Medium();
-//        medium2.setName("Another email");
-//        medium2.setType("email");
-//        tableData.add(medium2);
-//
-//        Medium medium3 = new Medium();
-//        medium3.setName("Home phone");
-//        medium3.setType("phone");
-//        tableData.add(medium3);
-
         return tableData;
     }
 
