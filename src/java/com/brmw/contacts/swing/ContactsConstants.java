785a9beb6ec8c5948a931b52413ff6369db44a49
Bruce Irving <bi@brmw.com>
2009-06-12 00:56:04 +0000

    Added keyword expansion.

--- src/java/com/brmw/contacts/swing/ContactsConstants.java
+++ src/java/com/brmw/contacts/swing/ContactsConstants.java
@@ -5,4 +5,5 @@ import java.awt.Font;
 public class ContactsConstants {
 
     public static final Font HEADER_FONT = new Font(null, Font.BOLD, 16);
+    public static final String REVISION = "$Revision$ ";
 }
