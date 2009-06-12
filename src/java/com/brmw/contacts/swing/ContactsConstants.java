e0fe8149f2987a6a51bcfdf022512b3a0dbb8657
Bruce Irving <bi@brmw.com>
2009-06-12 23:33:44 +0000

    Remove svn properties

--- src/java/com/brmw/contacts/swing/ContactsConstants.java
+++ src/java/com/brmw/contacts/swing/ContactsConstants.java
@@ -5,8 +5,8 @@ import java.awt.Font;
 public class ContactsConstants {
 
     public static final Font HEADER_FONT = new Font(null, Font.BOLD, 16);
-    public static final String REVISION = "$Revision$ ";
-    public static final String LAST_CHANGED_REVISION = "$LastChangedRevision$";
-    public static final String LAST_CHANGED_DATE = "$LastChangedDate$";
-    public static final String SVN_ID = "$Id$";
+    public static final String REVISION = "$Revision: 60 $ ";
+    public static final String LAST_CHANGED_REVISION = "$LastChangedRevision: 60 $";
+    public static final String LAST_CHANGED_DATE = "$LastChangedDate: 2009-06-11 20:01:04 -0500 (Thu, 11 Jun 2009) $";
+    public static final String SVN_ID = "$Id: ContactsConstants.java 60 2009-06-12 01:01:04Z bruce $";
 }
